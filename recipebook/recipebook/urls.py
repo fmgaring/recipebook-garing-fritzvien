879c7211ca2016197d9daf7617faf3a304207787
fmgaring <fritzvien.garing@student.ateneo.edu>
2024-03-04 17:42:02 +0800

    Lab 2 Commit.

--- recipebook/recipebook/urls.py
+++ recipebook/recipebook/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ledger/', include('ledger.urls', namespace='ledger'))
+    path('', include('ledger.urls', namespace='ledger'))
 ]
